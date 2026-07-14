c4a091426e02eea3137b086c87e22760010892c4
Jose Peñuela <269219523+joseferrer-webdevelopment@users.noreply.github.com>
2026-07-13 20:29:11 -0500

    feat: add playRound logic with win/lose/tie detection

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "Empate";
+  }
+
+  if (playerChoice === "piedra" && computerChoice === "tijera") {
+  return "Ganaste";
+}
+
+if (playerChoice === "papel" && computerChoice === "piedra") {
+  return "Ganaste";
+}
+
+if (playerChoice === "tijera" && computerChoice === "papel") {
+  return "Ganaste";
+}
+
+return "Perdiste"
+
+}
+
